f3cb736836fed82fa2bb46a02347cb19708c68de
HasanMufdy <96997047+HasanMufdy@users.noreply.github.com>
2023-06-14 12:15:24 +0300

    first commit

--- lab02-ATM/Program.cs
+++ lab02-ATM/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void main(string[] args)
+    {
+
+    }
+
+    //////////////////////////////////////
+    public static decimal ViewBalance()
+    {
+        decimal value = 0;
+        return value;
+    }
+
+    public static decimal WithDraw()
+    {
+        decimal takenMoney = 0;
+        return takenMoney;
+
+    }
+    
+    public static decimal Deposit()
+    {
+        decimal addedMoney = 0;
+        return addedMoney;
+    }
+
+
+}
